7b24902d98fbcd27dbc2476b85568badf17e4451
Taren Arns <tarenarns@outlook.com>
2026-02-26 17:23:11 -0600

    feat: fastapi websocket base

--- server/main.py
+++ server/main.py
@@ -1,8 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List, Dict
 
 app = FastAPI()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        self.user_map: Dict[WebSocket, str] = {}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    async def connect(self, websocket: WebSocket, username: str):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        self.user_map[websocket] = username
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        self.user_map.pop(websocket, None)
+
+    async def send_personal(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    async def broadcast(self, message: dict):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await manager.connect(websocket, username)
+
+    await manager.broadcast({
+        "type": "system",
+        "message": f"{username} joined"
+    })
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast({
+            "type": "system",
+            "message": f"{username} left"
+        })
